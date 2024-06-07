ed28b0732361e7afefed1dd500e522523667a33f
solidados <geekbrains.pk@internet.ru>
2024-06-07 14:11:58 +0400

    feat: create cdk-deployment-stack.ts and add cloudfrontOAI, Bucket and its policies, distribution, BucketDeployment

--- cdk-deployment/lib/cdk-deployment-stack.ts
+++ cdk-deployment/lib/cdk-deployment-stack.ts
@@ -1,16 +1,67 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import {
+  aws_cloudfront as cloudfront,
+  aws_s3 as s3,
+  aws_s3_deployment as s3deploy,
+  aws_iam as iam,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class CdkDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      "SolidadosStoreCloudfrontOAI",
+      {
+        comment: "OAI for SolidadosStoreCloudfront",
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDeploymentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const solidadosStoreBucket = new s3.Bucket(this, "SolidadosStoreBucket", {
+      bucketName: "solidados-store-bucket",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    solidadosStoreBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [solidadosStoreBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "SolidadosCloudfrontDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: solidadosStoreBucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    new s3deploy.BucketDeployment(this, "SolidadosBucketDeployment", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: solidadosStoreBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
