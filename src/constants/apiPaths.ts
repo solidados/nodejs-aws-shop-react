1bcd05eab218143c608de706f7a1c378dd28e5b4
solidados <geekbrains.pk@internet.ru>
2024-06-30 23:22:35 +0400

    refactor: update API GW URLs for import csv files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://7yqkyib6sc.execute-api.eu-central-1.amazonaws.com/prod",
+  order: "https://1izmos58lg.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://1izmos58lg.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://1izmos58lg.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://1izmos58lg.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
