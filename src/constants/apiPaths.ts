2cf6498f8c3866fcaca3784fc9aeb7119b69ff1c
solidados <geekbrains.pk@internet.ru>
2024-06-30 16:24:31 +0400

    refactor: update spiPaths.ts

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://4mxkirfv07.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://4mxkirfv07.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/prod",
+  order: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://mjugky0ydb.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
