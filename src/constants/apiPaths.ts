96b1946b64e8061dd7040264d943616502d8ab6b
solidados <geekbrains.pk@internet.ru>
2024-06-30 08:47:25 +0400

    feat: add URLs for import csv files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://7yqkyib6sc.execute-api.eu-central-1.amazonaws.com/prod",
+  product: "https://4mxkirfv07.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://4mxkirfv07.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
