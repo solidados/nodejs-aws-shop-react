6d73c70111611f92325106568b13cc8069631a8c
solidados <geekbrains.pk@internet.ru>
2024-06-30 08:10:39 +0400

    refactor: update CSVFileImport by add Get the pre-signed URL

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -10,38 +11,43 @@ type CSVFileImportProps = {
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const files: FileList | null = e.target.files;
     if (files && files.length > 0) {
-      const file = files[0];
+      const file: File = files[0];
+      console.log(`File upload: ${file}`);
       setFile(file);
     }
   };
 
-  const removeFile = () => {
+  const removeFile = (): void => {
     setFile(undefined);
   };
 
-  const uploadFile = async () => {
-    console.log("uploadFile to", url);
+  const uploadFile = async (): Promise<void> => {
+    console.log(`uploadFile to: ${url}`);
 
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    // Get the pre-signed URL
+    if (file) {
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+      });
+      console.log(`File to upload: ${file.name}`);
+      console.log(`Uploading to: ${response.data}`);
+
+      const result: Response = await fetch(response.data, {
+        method: "PUT",
+        body: file,
+      });
+      console.log(`Result: ${result}`);
+      setFile(undefined);
+    }
   };
+
   return (
     <Box>
       <Typography variant="h6" gutterBottom>
